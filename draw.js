b855fc8d35bfca390ce698097c622b2330fe67d0
CoderCowMoo <broskichad38@gmail.com>
2026-09-14 10:53:15 +0300

    Gaussian-like peaks near the middle working, now to add perlin noise

--- draw.js
+++ draw.js
@@ -26,16 +26,63 @@ function gaussianRandom(mean=0, stdev=1) {
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+
+// ----------- DRAWING
+
+// thanks to GPT-5.6 Sol (Medium)
+function peak(x, center, width, height) {
+    const distance = (x - center) / width;
+    // distance:   -3    -2    -1     0     1     2     3
+    // result:    .01   .14   .61    1.0   .61   .14   .01
+    return height * Math.exp(-0.5 * Math.pow(distance, 2))
+}
+
+// lets get perlin noise
+/**
+ * @returns Array
+ */
+function perlin1D() {
+    // get gradient vectors first for how many points though?
+    // let's say between 0 and canvas.width, we split it up into 14 lines?
+    // arbitrary choice
+    const num_grid_lines = 14
+    gradscalar = Array.from({length: num_grid_lines}, () => {Math.random()});
+
+    // 
+}
+
 // lets draw 30 lines right now
-for (let i = 0; i < 30; i++) {
+let num_lines = 25;
+for (let i = 0; i < num_lines; i++) {
     // starting x is obviously 0, and ending x is canvas.width?
     // convert a range of [0,30], to [canvas.height * 0.2, canvas.height * 0.8]
-    let y = (i / 30) * (canvas.height * 0.8 - canvas.height * 0.2) + (canvas.height * 0.2);
+    let y = (i / num_lines) * (canvas.height * 0.85 - canvas.height * 0.15) + (canvas.height * 0.15);
 
-    // let's find out if I done goofed.
-    ctx.beginPath();
-    ctx.strokeStyle = "white"
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.stroke();
+    // let's find out if I done goofed. YAYYYYY AMAZING
+    // now we need to work on drawing pixels from a line buffer.
+    // https://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
+    // amazing analysis here, but wow fill rect is faster than image data 1x1 and multiple times. A smart man would 
+    // be able to use image data easily, for the entire thing, but that man is not me.
+    // line_data will be an array of how many pixels up from y = 0, (or y = y) the line will be
+    const center = gaussianRandom(canvas.width / 5 * 2, 20);
+    const center2 = gaussianRandom(canvas.width / 5 * 3, 20);
+    const width = Math.max(20, gaussianRandom(60, 10));
+    const height = Math.max(10, gaussianRandom(80, 15));
+    const perlin_noise_1d = perlin1D();
+    let line_data = Array.from({length: canvas.width}, (_, x) => {
+        return -peak(x, center, width, height) -peak(x, center2, width, height) + perlin_noise_1d[x];
+    });
+
+    for (let j = 0; j < canvas.width; j++) {
+        let yval = line_data[j];
+        ctx.fillStyle = "white";
+        ctx.fillRect(j, y + yval, 1, 1);
+        // fill underneath
+        ctx.fillStyle = "black";
+        ctx.fillRect(j, y + yval + 1, 1, canvas.height - (y + yval + 1));
+    }
+    
+
+    // after each line draw black underneath. oh this wouldn't work :( need to do so under the entire vertical
+    // slice.
 }
---
+++ thoughts.md
@@ -0,0 +1,16 @@
+I need to relate j, which is the x position of the line, to the gaussian distribution.
+
+Somehow multiple dists, lets say 3, must increase in significance as we get closer to 
+3 random points, but until we get to those points, it should be near 0. How do you do that? lets say the
+random point is 260. j / 260 would be 0 at the start, then would be nearly half at the halfway point, then 1 at the full point, but then after 1 we need to subtract the point. E.g. 340 / 260 ~~ 1.31, so we would do 340 / 260 - (Math.trunc(340 / 260 - 340 // 260).
+nevermind. 
+
+```js
+function smooth(x, target) {
+    let ratio = (x % target) / target;
+    return (ratio - Math.trunc(ratio))*2
+}
+
+```
+
+Best i got before asking codex.
